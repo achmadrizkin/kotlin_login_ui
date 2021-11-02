df9911df4038818b302b7696390eaeb4ea90410e
achmadrizkin <arizki.nf02@gmail.com>
2021-11-02 12:49:28 +0700

    Finish

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -1,11 +1,203 @@
 package com.example.composeui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.composeui.theme.DefaultTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            DefaultTheme {
+                SignInScreen()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SignInScreen()
+}
+
+@Composable
+fun SignInScreen() {
+    var username by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var isPasswordVisible by remember {
+        mutableStateOf(false)
+    }
+    val isFormValid by derivedStateOf {
+        username.isNotBlank() && password.length >= 7
+    }
+
+    Scaffold(backgroundColor = MaterialTheme.colors.primary) {
+        Column(
+            Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Top
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.music),
+                contentDescription = "App logo",
+                modifier = Modifier.weight(1f).size(200.dp),
+                colorFilter = ColorFilter.tint(Color.White)
+            )
+
+            //
+            Card(
+                Modifier.weight(2f).padding(8.dp),
+                shape = RoundedCornerShape(32.dp)
+            ) {
+                Column(
+                    Modifier.fillMaxSize().padding(32.dp)
+                ) {
+                    Text(text = "Welcome Back", fontWeight = FontWeight.Bold, fontSize = 24.sp)
+                    Text(
+                        text = "Nice to see you again",
+                        fontWeight = FontWeight.Normal,
+                        fontSize = 14.sp,
+                        color = Color.Gray
+                    )
+
+                    Column(
+                        Modifier.fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = username,
+                            onValueChange = { username = it },
+                            label = { Text(text = "Username") },
+                            singleLine = true,
+                            trailingIcon = {
+                                if (username.isNotBlank()) {
+                                    IconButton(onClick = { username = "" }) {
+                                        Icon(imageVector = Icons.Filled.Clear, contentDescription = "")
+                                    }
+                                }
+                            }
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            value = password,
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Password,
+                                imeAction = ImeAction.Done
+                            ),
+                            visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            onValueChange = { password = it },
+                            label = { Text(text = "Password") },
+                            singleLine = true,
+                            trailingIcon = {
+                                if (password.isNotBlank()) {
+                                    IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
+                                        Icon(
+                                            imageVector = if (isPasswordVisible) Icons.Default.Visibility else Icons.Default.VisibilityOff,
+                                            contentDescription = "Password Toggle"
+                                        )
+                                    }
+                                }
+                            }
+                        )
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        if (isFormValid) {
+                            Button(
+                                onClick = {},
+                                enabled = isFormValid,
+                                modifier = Modifier.fillMaxWidth(),
+                                shape = RoundedCornerShape(10.dp)
+                            ) {
+                                Text(text = "Log In")
+                            }
+                        } else if (username.isBlank() && password.length > 7) {
+                            Text(
+                                text = "Please input Username",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 14.sp,
+                                color = Color.Gray
+                            )
+                        } else if (password.length < 7 && password.isNotBlank()) {
+                            Text(
+                                text = "Please input Password more than 7 Characters",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 14.sp,
+                                color = Color.Gray,
+                                textAlign = TextAlign.Center
+                            )
+                        } else if (username.isNotBlank()) {
+                            Text(
+                                text = "Please input Password",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 14.sp,
+                                color = Color.Gray
+                            )
+                        } else if (username.isBlank() && password.isBlank()) {
+                            Text(
+                                text = "Please input Username, and Password",
+                                fontWeight = FontWeight.Normal,
+                                fontSize = 14.sp,
+                                color = Color.Gray
+                            )
+                        }
+
+
+                        Spacer(modifier = Modifier.weight(1f))
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+                            TextButton(onClick = {}) {
+                                Text(text = "Sign Up")
+                            }
+
+                            TextButton(onClick = {}) {
+                                Text(text = "Forgot Password?", color = Color.Gray)
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
     }
 }
